ba002ce9f0b10b2d41ac0ed99bd77e5b17f26c10
vlong638 <vlong638@163.com>
2016-11-10 17:47:22 +0800

    1110

---
+++ VL.ItsMe1110/Attributes/CustomAuthAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VL.ItsMe1110.Attributes
+{
+    /// <summary>
+    /// 用于测试AuthorizeAttribute
+    /// [CustomAuth(false)]
+    /// 拦截本地请求,一般不需要自定制
+    /// MVC标准实现为
+    /// [Authorize(Order = -1,Roles = "", Users = "")]
+    /// </summary>
+    public class CustomAuthAttribute: AuthorizeAttribute
+    {
+        private bool localAllowed;
+        public CustomAuthAttribute(bool allowedParam)
+        {
+            localAllowed = allowedParam;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.IsLocal)
+            {
+                return localAllowed;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
--- VL.ItsMe1110/Controllers/HomeController.cs
+++ VL.ItsMe1110/Controllers/HomeController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VL.ItsMe1110.Attributes;
 using VL.ItsMe1110.Controllers;
 
 namespace VL.ItsMe1107.Controllers
 {
     public class HomeController : BaseController
     {
-
         public ActionResult Index()
         {
             return View();
