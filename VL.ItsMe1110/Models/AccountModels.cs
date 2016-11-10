48a400adba1a1450a75c771c6309088af9e15806
vlong638 <vlong638@163.com>
2016-11-10 16:10:15 +0800

    启用Model特性验证

--- VL.ItsMe1110/Models/AccountModels.cs
+++ VL.ItsMe1110/Models/AccountModels.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace VL.ItsMe1110.Models
 {
     public class RegisterViewModel
     {
         [Display(Name = "用户名")]
-        //[Required]
+
+        [Required]
         public string UserName { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "密码")]
-        //[Required]
-        //[StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "确认密码")]
-        //[Compare("Password", ErrorMessage = "密码和确认密码不匹配。")]
+
+        [Compare(nameof(Password), ErrorMessage = "密码和确认密码不匹配。")]
         public string ConfirmPassword { get; set; }
 
-        //[EmailAddress]
     }
 }
