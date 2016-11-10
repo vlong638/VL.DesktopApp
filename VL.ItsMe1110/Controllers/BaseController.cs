a4786ab72b746374b5c0c2bac28e47e2a1169355
vlong638 <vlong638@163.com>
2016-11-10 13:50:44 +0800

    ItsMe1110

---
+++ VL.ItsMe1110/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace VL.ItsMe1110.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public const string PageNameOfError = "error";
+        public const string PageNameOfHome = "Home";
+        public const string PageNameOfAccount = "Account";
+        public const string PageNameOfManage = "Manage";
+    }
+}
